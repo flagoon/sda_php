510f1b7816aba4d98826a483d8326cae4f4e779d
Paweł Kochanek <flagoon@o2.pl>
2017-11-28 21:37:32 +0100

    Cleaning some styles

--- react-geodist/src/components/Input.js
+++ react-geodist/src/components/Input.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FormControl, Col, Row, Grid } from 'react-bootstrap';
-import $ from 'jquery';
 
 class Input extends React.Component {
   constructor() {
@@ -10,33 +8,19 @@ class Input extends React.Component {
     };
   }
 
-  getAjax() {
-    $.ajax({
-      url: 'find_city.php',
-      data: {
-        cont: this.
-      }
-    }).done({
+  // getAjax() {
+  //   $.ajax({
+  //     url: 'find_city.php',
+  //     data: {
+  //       cont: this.
+  //     }
+  //   }).done({
 
-    })
-  };
+  //   })
+  // };
 
   render () {
-    return (
-      <Grid bsStyle="mt-2">
-        <Row>
-          {
-            this.state.cities.map((val, index) => {
-              return (
-                <Col xs={2}>
-                  <FormControl key={index} placeholder= "Wpisz miasto" id={val} onKeyUp={this.getAjax}/>
-                </Col>
-              );
-            })
-          }
-        </Row>
-      </Grid>
-    );
+    return ;
   }
 }
 
