4262653a63366cb587a4bc312ecf20920d20df45
Paweł Kochanek <flagoon@o2.pl>
2017-11-28 21:39:39 +0100

    removing all Input

--- react-geodist/src/App.js
+++ react-geodist/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Input from './components/Input';
 
 class App extends React.Component {
   render() {
     return (
-      <Input name="Paweł"/>
+
     );
   }
 }
