b823a6ebcfb9733ba38a4f6dd695fb8b497985ec
Paweł Kochanek <flagoon@o2.pl>
2017-11-30 23:04:45 +0100

    nothing changed

--- weatherapi/src/index.js
+++ weatherapi/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.css';
+
+//ignore next line
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
