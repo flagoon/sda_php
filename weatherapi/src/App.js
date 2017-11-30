e77bdb9ca90a6b5c56ca5133275c65989850340e
Paweł Kochanek <flagoon@o2.pl>
2017-11-30 23:06:13 +0100

    Nothing changed

--- weatherapi/src/App.js
+++ weatherapi/src/App.js
@@ -2,18 +2,10 @@ import React from 'react';
 import WeatherContainer from './components/WeatherContainer';
 
 class App extends React.Component {
-  constructor () {
-    super();
-    this.state = {
-      weather: {
-        bgPicture: ['clear_sky.jpeg', 'rain.jpeg', 'mist.jpeg', 'snow.jpeg', 'heavy_clouds.jpeg'] 
-      }
-    };
-  }
 
   render() {
     return (
-      <WeatherContainer backgroundPicture={this.state.weather.bgPicture[4]}/>
+      <WeatherContainer />
     );
   }
 }
