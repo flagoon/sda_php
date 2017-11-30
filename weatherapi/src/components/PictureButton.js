f0305b414974e11f860092fb15c749921d8da0d4
Paweł Kochanek <flagoon@o2.pl>
2017-11-30 13:06:37 +0100

    Added button to try some events. Failed :(

---
+++ weatherapi/src/components/PictureButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PictureButton extends React.Component {
+  render() {
+    return (
+      <div className="random-bg" ref="random-button">
+        Losuj
+      </div>
+    );
+  }
+}
+
+export default PictureButton;
