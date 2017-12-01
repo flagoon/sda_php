2b7bc26f6b49845dbb709495d4f19acdef01a7e3
Paweł Kochanek <flagoon@o2.pl>
2017-12-01 23:14:34 +0100

    Added ajax

--- weatherapi/src/components/WeatherContainer.js
+++ weatherapi/src/components/WeatherContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FromCity from './FromCity';
 import PictureButton from './PictureButton';
+import $ from 'jquery';
 
 class WeatherContainer extends React.Component {
   constructor () {
@@ -9,8 +10,53 @@ class WeatherContainer extends React.Component {
       weather: {
         bgPicture: ['clear_sky.jpeg', 'rain.jpeg', 'mist.jpg', 'snow.jpeg', 'heavy_clouds.jpeg', 'clouds.jpeg'] 
       },
-      randomBG: 1
+      coords: {
+        lat: '',
+        lon: ''
+      }
     };
+  }
+
+  componentDidMount() {
+    this.getGeoLocation();
+    this.getWeatherData();
+  }
+
+  getGeoLocation() {
+
+    //check if browser can get geolocation
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        //get lat ang lon as obj
+        this.setState({coords:
+            {
+              lat: pos.coords.latitude,
+              lon: pos.coords.longitude
+            }
+        });
+        console.log(this.state)
+        //alert when error happens
+      }, (error) => {
+        alert('There was an error: ' + error.message);
+      });
+    }
+    
+  }
+
+  getWeatherData() {
+    console.log(this.state.coords.lat)
+    $.ajax({
+      url: 'https://fcc-weather-api.glitch.me/',
+      data: {
+        lat: this.state.coords.lat,
+        lon: this.state.coords.lon
+      },
+      dataType: 'JSON'
+    }).done((data) => {
+      console.log(data);
+    }).fail((one, two, err) => {
+      console.log(err);
+    });
   }
 
   getBackgroundData (data) {
@@ -18,7 +64,7 @@ class WeatherContainer extends React.Component {
   }
 
   render() {
-    var style = {
+    let style = {
       backgroundImage: `url(./weatherPictures/${this.state.weather.bgPicture[this.state.randomBG]})`,
     };
     return (
