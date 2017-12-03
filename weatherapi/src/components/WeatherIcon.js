67a5de52b8702e7bdfd6aa7ead9dddb033a84d9c
Paweł Kochanek <flagoon@o2.pl>
2017-12-03 20:27:28 +0100

    added weather icon for react and also an WeatherIcon component

---
+++ weatherapi/src/components/WeatherIcon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WI from 'react-weathericons';
+
+class WeatherIcon extends React.Component {
+
+  constructor () {
+    super();
+    this.state = {
+      mainIcon: ''
+    };
+  }
+}
+
+export default WeatherIcon;
