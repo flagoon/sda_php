66c195e280c00269fd65d6e24ece021f761f6273
Paweł Kochanek <flagoon@o2.pl>
2017-12-07 20:49:41 +0100

    added condition for WeatherIcon. If degrees are given, show degrees

--- weatherapi/src/components/WeatherIcon.js
+++ weatherapi/src/components/WeatherIcon.js
@@ -4,9 +4,17 @@ import WI from 'react-weathericons';
 class WeatherIcon extends React.Component {
 
   render () {
+
+    let degrees = null;
+
+    if (this.props.value) {
+      degrees = <div>{this.props.value}</div>;
+    }
+
     return (
-      <div className="big-icon-div">
-        <WI name={'owm-' + this.props.iconName} />
+      <div>
+        {degrees}
+        <WI name={this.props.iconName} />
       </div>
     );
   }
