8fc0857fa9117b3d3811a8db466e413dc32a40b1
Pawel Kochanek <pawel.kochanek@avid.com>
2017-12-08 12:24:37 +0100

    Change ajax request to fetchAPI. Change getGeoLocation as promised function

--- weatherapi/src/components/WeatherContainer.js
+++ weatherapi/src/components/WeatherContainer.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import FromCity from './FromCity';
-// import PictureButton from './PictureButton';
 import WeatherIcon from './WeatherIcon';
 import SmallWeather from './SmallWeather';
-import $ from 'jquery';
 
 class WeatherContainer extends React.Component {
-  constructor () {
+  constructor() {
     super();
+    this.getGeoLocation = this.getGeoLocation.bind(this);
+    this.getWeatherData = this.getWeatherData.bind(this);
     this.state = {
       weather: {
-        bgPicture: ['clear_sky.jpeg', 'rain.jpeg', 'mist.jpg', 'snow.jpeg', 'heavy_clouds.jpeg', 'clouds.jpeg'] 
-      },
-      coords: {
-        lat: '',
-        lon: ''
+        bgPicture: ['clear_sky.jpeg', 'rain.jpeg', 'mist.jpg', 'snow.jpeg', 'heavy_clouds.jpeg', 'clouds.jpeg']
       },
       weatherData: {
         cityName: '',
@@ -30,64 +26,57 @@ class WeatherContainer extends React.Component {
   }
 
   componentDidMount() {
-    this.getGeoLocation();
+    this.getGeoLocation().then(res => {
+      this.getWeatherData(res);
+    }).catch((error) => {
+      this.getWeatherData(error);
+    });
   }
 
   getGeoLocation() {
-
-    //check if browser can get geolocation
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((pos) => {
-        //get lat ang lon as obj
-        this.setState({coords:
-            {
+    return new Promise((res, rej) => {
+      //check if browser can get geolocation
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((pos) => {
+          //get lat ang lon as obj
+          res({
+            coords: {
               lat: pos.coords.latitude,
               lon: pos.coords.longitude
             }
+          });
+          //alert when error happens
+        }, (error) => {
+          //when geolocation of browser is turnoff, set Warsaw location.
+          rej({
+            coords: {
+              lat: 52.22,
+              lon: 21.01
+            }
+          });
         });
-        this.getWeatherData();
-        //alert when error happens
-      }, (error) => {
-        //
-        alert('Twoja przeglądarka nie obsługuje geolokacji. Ustawiono domyślną wartość.');
-        this.setState({coords:
-          {
-            lat: 52.22,
-            lon: 21.01
-          }
-        });
-        this.getWeatherData();
-      });
-    }
-    
+      }
+    });
   }
 
-  getWeatherData() {
-    $.ajax({
-      url: 'https://fcc-weather-api.glitch.me/api/current',
-      data: {
-        lat: this.state.coords.lat,
-        lon: this.state.coords.lon
-      },
-      dataType: 'html',
-      type: 'get'
-    }).done((data) => {
-      const parsedData = JSON.parse(data);
-      this.setState({
-        weatherData: {
-          cityName: parsedData.name,
-          temp: parsedData.main.temp,
-          humidity: parsedData.main.humidity,
-          pressure: parsedData.main.pressure,
-          sunrise: this.getHoursAndMinutes(parsedData.sys.sunrise),
-          sunset: this.getHoursAndMinutes(parsedData.sys.sunset),
-          id: 'owm-' + parsedData.weather[0].id
-        },
-        degrees: 'celsius'
+  getWeatherData(x) {
+    fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${x.coords.lat}&lon=${x.coords.lon}`)
+      .then(data => data.json())
+      .then(data => {
+        this.setState({
+          weatherData: {
+            cityName: data.name,
+            temp: data.main.temp,
+            humidity: data.main.humidity,
+            pressure: data.main.pressure,
+            sunrise: this.getHoursAndMinutes(data.sys.sunrise),
+            sunset: this.getHoursAndMinutes(data.sys.sunset),
+            id: 'owm-' + data.weather[0].id
+          },
+          degrees: 'celsius'
+        });
       });
-    }).fail((hxr, status, err) => {
-      console.log(err);
-    });
+    
   }
 
   getHoursAndMinutes(value) {
@@ -100,24 +89,23 @@ class WeatherContainer extends React.Component {
     let style = {
       backgroundImage: `url(./weatherPictures/${this.state.weather.bgPicture[this.state.randomBG]})`,
     };
-    return (
-      <div id="weather-container" className="weather-container" style={style}>
-        <FromCity cityName={this.state.weatherData.cityName} />
-        <div className="big-container">
-          <WeatherIcon iconName={this.state.weatherData.id} />
-          <WeatherIcon iconName={this.state.degrees} value={this.state.weatherData.temp}/>
-        </div>
-        <div className="small-parts">
-          {/*ciśnienie*/}
-          <SmallWeather icon="humidity" value={this.state.weatherData.humidity + '%'}/>
-          {/*wilgotność*/}
-          <SmallWeather icon="barometer" value={this.state.weatherData.pressure}/>
-          {/*wschód*/}
-          <SmallWeather icon="sunrise" value={this.state.weatherData.sunrise}/>
-          {/*zachód*/}
-          <SmallWeather icon="sunset" value={this.state.weatherData.sunset}/>
-        </div>
-        {/*<PictureButton getData={this.getBackgroundData.bind(this)}/>*/}
+    return ( 
+      <div id = "weather-container" className = "weather-container" style = {style} >
+        <FromCity cityName = {this.state.weatherData.cityName}/> 
+        <div className = "big-container" >
+          <WeatherIcon iconName = {this.state.weatherData.id}/> 
+          <WeatherIcon iconName = {this.state.degrees} value = {this.state.weatherData.temp}/> 
+        </div> 
+        <div className = "small-parts" > 
+          { /*ciśnienie*/ } 
+          <SmallWeather icon = "humidity" value = {this.state.weatherData.humidity + '%'}/> 
+          {/* wilgotność */} 
+          <SmallWeather icon = "barometer" value = {this.state.weatherData.pressure}/> 
+          { /* wschód */ } 
+          <SmallWeather icon = "sunrise" value = {this.state.weatherData.sunrise}/> 
+          { /* zachód */ } 
+          <SmallWeather icon = "sunset" value = {this.state.weatherData.sunset} /> 
+        </div> 
       </div>
     );
   }
