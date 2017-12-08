8730b3d7e1c3a26279d10e815b25a15be3169cd2
Pawel Kochanek <pawel.kochanek@avid.com>
2017-12-08 15:06:39 +0100

    Start working around Celcius to Fahrenhait conversion

--- weatherapi/src/components/WeatherIcon.js
+++ weatherapi/src/components/WeatherIcon.js
@@ -2,6 +2,24 @@ import React from 'react';
 import WI from 'react-weathericons';
 
 class WeatherIcon extends React.Component {
+  constructor() {
+    super ();
+    this.state = {
+      degrees: {
+        value: '',
+        type: ''
+      }
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      degrees: {
+        value: this.props.value,
+        type: this.props.iconName
+      }
+    });
+  }
 
   render () {
 
@@ -13,7 +31,9 @@ class WeatherIcon extends React.Component {
 
     return (
       <div>
+        {/* value for temperature */}
         {degrees}
+        {/* celisius icon is sent by default */}
         <WI name={this.props.iconName} />
       </div>
     );
