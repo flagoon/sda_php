17cd727669ed437f3c642419d67845213ca391fa
Paweł Kochanek <flagoon@o2.pl>
2017-12-09 22:58:49 +0100

    added function to convert celsius to fahrneheit

--- weatherapi/src/components/Helpers.js
+++ weatherapi/src/components/Helpers.js
@@ -3,3 +3,16 @@ export const getHoursAndMinutes = (value) => {
   time.setTime(value * 1000);
   return `${time.toLocaleString([], {hour: '2-digit', minute:'2-digit', hour12: false})}`;
 };
+
+export const changeTemperatureScale = (degrees, scale) => {
+  let weatherIconContainer = null;
+  let temperature = degrees;
+  if (scale === 'celsius') {
+    temperature = Number(degrees) * 1.8 + 32;
+    weatherIconContainer = <WeatherIcon iconName = 'fahrneheit' value = {temperature}/>;
+  } else {
+    temperature = (Number(degrees) - 32) / 1.8;
+    weatherIconContainer = <WeatherIcon iconName = 'celsius' value = {temperature}/>;
+  }
+  return weatherIconContainer;
+};
