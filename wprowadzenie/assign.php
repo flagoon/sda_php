c6e962831cd3b9ab4a499237da279df0906dc944
Paweł Kochanek <flagoon@o2.pl>
2017-12-10 10:56:44 +0100

    added coments to  describe variables

--- wprowadzenie/assign.php
+++ wprowadzenie/assign.php
@@ -1,6 +1,11 @@
 <?php
+# this is the name of user
 $name = 'Bobik';
+
+# this is the number of cleaned rooms
 $roomsCounter = 13;
+
+# the fuck I know
 $currentBandName = $name;
 
 echo $name . "\n";
