229272e3e611bc3bf84d27b26deac726f4e3c564
Paweł Kochanek <flagoon@o2.pl>
2017-12-10 12:26:10 +0100

    change one math

--- wprowadzenie/simple.php
+++ wprowadzenie/simple.php
@@ -3,5 +3,5 @@ echo (4 == 3) ? 4 : 3, PHP_EOL;
 echo (5 - 2), PHP_EOL;
 echo (30 * 0.1), PHP_EOL;
 echo (27 / 9), PHP_EOL;
-echo (9 ** 0.5), PHP_EOL;
+echo (27 ** (1/3)), PHP_EOL;
 echo (15 % 6), PHP_EOL;
