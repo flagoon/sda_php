9823400b3341f310546cb04f56a01042c6028182
Paweł Kochanek <flagoon@o2.pl>
2017-12-10 10:24:03 +0100

    changed single to double apostroph

--- wprowadzenie/hello.php
+++ wprowadzenie/hello.php
@@ -1,8 +1,8 @@
 <?php 
-echo 'Hello, World!';
-echo 'Oto tekst
-zlozony z kilku linii.';
-echo 'Dodajemy \'apostrof\' do stringa.';
-echo 'Znak nowej linii to \n';
-echo 'Zmienna to $zmienna';
+echo "Hello, World!";
+echo "Oto tekst
+zlozony z kilku linii.";
+echo "Dodajemy \'apostrof\' do stringa.";
+echo "Znak nowej linii to \n";
+echo "Zmienna to $zmienna";
 
