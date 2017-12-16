f4f4757327adb94396ff72450067579f50ad1544
Paweł Kochanek <flagoon@o2.pl>
2017-12-16 13:28:05 +0100

    added switch case for china year

--- variables/china.php
+++ variables/china.php
@@ -7,3 +7,50 @@ $rok = readline("Podaj rok urodzenia: ");
 $znak = $rok % 12;
 
 echo "Jesteś spod znaku {$china[$znak]}", PHP_EOL;
+
+switch ($znak) {
+    case 0:
+        echo "{$china[$znak]}";
+        break;
+    case 1:
+        echo "{$china[$znak]}";
+        break;   
+    case 2:
+        echo "{$china[$znak]}";
+        break;
+    case 3:
+        echo "{$china[$znak]}";
+        break; 
+    case 4:
+        echo "{$china[$znak]}";
+        break;
+    case 5:
+        echo "{$china[$znak]}";
+        break;  
+    case 6:
+        echo "{$china[$znak]}";
+        break;
+    case 7:
+        echo "{$china[$znak]}";
+        break; 
+    case 8:
+        echo "{$china[$znak]}";
+        break;   
+    case 9:
+        echo "{$china[$znak]}";
+        break;
+    case 10:
+        echo "{$china[$znak]}";
+        break; 
+    case 11:
+        echo "{$china[$znak]}";
+        break;
+    case 12:
+        echo "{$china[$znak]}";
+        break; 
+    default:
+        echo "Coś poszło nie tak!";
+        break;
+}
+
+echo PHP_EOL;
