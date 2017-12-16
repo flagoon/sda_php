bffe9754345c1ecb858a1eff470789fde08b6ea5
Paweł Kochanek <flagoon@o2.pl>
2017-12-16 12:38:36 +0100

    added colors and rounded MB

--- variables/memory.php
+++ variables/memory.php
@@ -2,8 +2,10 @@
 
 $allocatedMemory = memory_get_usage();
 
-if (640 > ($allocatedMemory / 1024)) {
+if (240 > ($allocatedMemory / 1024)) {
     echo "640 KB wystarczy każdemu.", PHP_EOL;
+} else {
+    echo "Potrzebujesz \033[31m", round((($allocatedMemory/1024)/1024), 2), "MB.\033[0m", PHP_EOL;
 }
 
-echo 'Używasz: ', (int)($allocatedMemory / 1024) , 'KB i ', ($allocatedMemory % 1024), 'B.' , PHP_EOL;
+echo "Używasz: \033[33m", (int)($allocatedMemory / 1024) , "KB\033[0m i \033[33m", ($allocatedMemory % 1024), "B\033[0m." , PHP_EOL;
