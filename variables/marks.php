9624bc0df48159cb108a85e65c8280d8d660f113
Paweł Kochanek <flagoon@o2.pl>
2017-12-16 14:32:10 +0100

    created array with pupils and scores

---
+++ variables/marks.php
@@ -0,0 +1,24 @@
+<?php
+
+$uczniowie = [
+    "Zuzia" => [
+        "j. polski" => 5,
+        "biologia" => 5,
+        "j. angielski" => 5,
+        "historia" => 5,
+    ],
+    "Alicja" => [
+        "j. polski" => 3,
+        "biologia" => 2,
+        "j. angielski" => 4,
+        "historia" => 5,
+    ],
+    "Bobik" => [
+        "j. polski" => 5,
+        "biologia" => 2,
+        "j. angielski" => 2,
+        "historia" => 4,
+    ],
+];
+
+var_export($uczniowie);
