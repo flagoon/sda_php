885476a9c1096ae47fffde8f370e03c913222c21
Paweł Kochanek <flagoon@o2.pl>
2017-12-16 14:51:00 +0100

    added borders

--- variables/marks.php
+++ variables/marks.php
@@ -1,5 +1,7 @@
 <?php
 
+system('clear');
+
 $uczniowie = [
     "Zuzia" => [
         "j. polski" => 5,
@@ -21,4 +23,21 @@ $uczniowie = [
     ],
 ];
 
-var_export($uczniowie);
+$arraySize = count($uczniowie);
+
+foreach ($uczniowie as $uczen => $oceny) {
+    echo "Uczen: \033[33m$uczen\033[0m", PHP_EOL;
+    echo "\033[40m*************************\033[0m", PHP_EOL;
+    
+    foreach ($oceny as $przedmiot => $ocena) {
+        echo " $przedmiot $ocena";
+        $spaceVar = 22 - mb_strlen($przedmiot) - mb_strlen($ocena);
+        
+        for ($i = 0; $i < $spaceVar; $i++) {
+            echo " ";
+        }
+        echo "\033[40m*\033[0m", PHP_EOL;
+    }
+
+    echo PHP_EOL;
+}
