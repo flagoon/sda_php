c47990f82137711d76f082e5f14ca69b20c4ec68
Paweł Kochanek <flagoon@o2.pl>
2017-12-16 09:36:52 +0100

    added else condition to sing.php

--- work-vars/sign.php
+++ work-vars/sign.php
@@ -3,5 +3,7 @@
     if (gettype($variableToCheck) == integer || gettype($variableToCheck) == double) {
         $condition = ($variableToCheck < 0) ? 'UJEMNA.' : 'DODATNIA.';
         echo 'Liczba ' . $variableToCheck . ' jest ' . $condition, PHP_EOL;
+    } else {
+        echo "Variable should be a number.";
     }
     
