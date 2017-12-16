07f4923b2a59dd0fd2b93eba11f31c1cddbec38b
Paweł Kochanek <flagoon@o2.pl>
2017-12-16 09:24:04 +0100

    added condition to check type of variable

--- work-vars/sign.php
+++ work-vars/sign.php
@@ -1,4 +1,7 @@
 <?php 
-    $variableToCheck = -5;
-    $condition = ($variableToCheck < 0) ? 'ujemna.' : 'dodatnia.';
-    echo 'Liczba jest ' . $condition, PHP_EOL;
+    $variableToCheck = -5.0;
+    if (gettype($variableToCheck) == integer || gettype($variableToCheck) == double) {
+        $condition = ($variableToCheck < 0) ? 'ujemna.' : 'dodatnia.';
+        echo 'Liczba jest ' . $condition, PHP_EOL;
+    }
+    
