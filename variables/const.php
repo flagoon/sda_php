5f3417d920469853f6d4c659d40c44eaaa30c7ba
Paweł Kochanek <flagoon@o2.pl>
2017-12-17 09:58:47 +0100

    created file with constants

---
+++ variables/const.php
@@ -0,0 +1,8 @@
+<?php
+echo E_ALL, PHP_EOL;
+echo __LINE__, PHP_EOL;
+echo __FILE__, PHP_EOL;
+echo __DIR__, PHP_EOL;
+echo __FUNCTION__, PHP_EOL;
+echo __CLASS__, PHP_EOL;
+echo __NAMESPACE__, PHP_EOL;
