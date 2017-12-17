9ad28f856f68eb3a5b597c3cf564c481e0f794b8
Paweł Kochanek <flagoon@o2.pl>
2017-12-17 10:49:45 +0100

    replace str functions to mb_str

--- functions/fun_string.php
+++ functions/fun_string.php
@@ -5,14 +5,14 @@ system('clear');
 $strings = ['Navicat', 'Category', 'Catharsis'];
 
 foreach ($strings as $string) {
-    if (stripos($string, 'cat') > -1) {
-        echo "Pozycja: ",$string, " ", stripos($string, 'cat'), PHP_EOL;
+    if (mb_stripos($string, 'cat') > -1) {
+        echo "Pozycja: ",$string, " ", mb_stripos($string, 'cat'), PHP_EOL;
     }
 
     echo "Długość: ", $string, " ", strlen($string), PHP_EOL;
 
-    if (stripos($string, 'cat') > -1) {
-        $newString = str_replace('cat', 'dog', strtolower($string));
+    if (mb_stripos($string, 'cat') > -1) {
+        $newString = mb_ereg_replace ('/cat/gi', 'dog', $string);
         echo "Zamienniono: ", $newString, " ", PHP_EOL;
     }
     echo "**************************", PHP_EOL;
