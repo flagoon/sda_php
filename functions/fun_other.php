e068c85954e99b9d35cb99dc70b9c32afd9b4cc3
Paweł Kochanek <flagoon@o2.pl>
2017-12-17 11:50:55 +0100

    test on json_encode

--- functions/fun_other.php
+++ functions/fun_other.php
@@ -1,8 +1,15 @@
 <?php
 
-$countries = ['Russia', 'Italy', true, 4.4];
+$countries = [
+    'name' => 'Russia',
+    'capitol' => 'Moscow', 
+    'hasPresident' => true, 
+    'prisons' => 4.4];
 
 $serializeData = serialize($countries);
 
 echo $serializeData, PHP_EOL;
 
+$jsonData = json_encode($countries);
+
+echo $jsonData, PHP_EOL;
