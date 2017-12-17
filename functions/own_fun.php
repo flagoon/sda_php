28d10593994676a9fc94b7ad7a9bf2ab13df28da
Paweł Kochanek <flagoon@o2.pl>
2017-12-17 12:09:16 +0100

    added documentation

--- functions/own_fun.php
+++ functions/own_fun.php
@@ -2,10 +2,15 @@
 declare(strict_types = 1);
 system('clear');
 
+/**
+ * function to echo formatted family name
+ * 
+ * @param string $familyName nazwisko
+ */
 function printFamilyName(string $familyName): void 
 {
     echo "Name: $familyName", PHP_EOL;
 }
 
-printFamilyName(23423423);
+printFamilyName("Kochanek");
 printFamilyName("Kowalski");
