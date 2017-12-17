de87218ff0afd6d5658eec20f5e50821e0c8d13b
Paweł Kochanek <flagoon@o2.pl>
2017-12-17 09:24:44 +0100

    change break to continue - infinite loop

--- variables/fruits.php
+++ variables/fruits.php
@@ -9,7 +9,7 @@ for ($i = 0; $i < 2; ++$i) {
 
         while ($boxMax) {
             if ($fruit === 'Orange') {
-                break;
+                continue;
             }
             $box[] = $fruit;
             --$boxMax;
