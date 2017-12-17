e9638640f2bb43fea9a7d6c364d17437802202db
Paweł Kochanek <flagoon@o2.pl>
2017-12-17 10:03:07 +0100

    added more constants

--- variables/const.php
+++ variables/const.php
@@ -6,3 +6,8 @@ echo __DIR__, PHP_EOL;
 echo __FUNCTION__, PHP_EOL;
 echo __CLASS__, PHP_EOL;
 echo __NAMESPACE__, PHP_EOL;
+
+echo PHP_VERSION, PHP_EOL;
+echo PHP_OS, PHP_EOL;
+echo PHP_INT_MAX, PHP_EOL;
+echo DEFAULT_INCLUDE_PATH, PHP_EOL;
