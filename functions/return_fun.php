ea8127cf4ab9969867ac1dd26dc306fcaed0848f
Paweł Kochanek <flagoon@o2.pl>
2017-12-17 12:26:40 +0100

    added documentation

--- functions/return_fun.php
+++ functions/return_fun.php
@@ -2,6 +2,13 @@
 declare(strict_types=1);
 system('clear');
 
+/**
+ * Simple function to return a sum of two numbers
+ * 
+ * @param int $a [opt] first number to sum
+ * @param int $b [opt] second number to sum
+ * @return int result of sum
+ */
 function sum(int $a = 0, int $b = 0): int
 {
     return $a + $b;
