3c7e1de83abc7197adc48ec1149fc8cfd4e7f323
Paweł Kochanek <flagoon@o2.pl>
2017-12-18 21:42:29 +0100

    explain the logic.

--- merryChristmass/palindrom.php
+++ merryChristmass/palindrom.php
@@ -9,3 +9,12 @@
 declare(strict_types=1);
 system('clear');
 
+/**
+ * Main idea is as follows. I check first and last letter of the string. If they are the same
+ * I will check the first and last letter of string that's between those letter. If I reach situation
+ * that this substring length is equal 1 or 0, I will return true. That way I should have the longest
+ * palindrom. If in some point first and last letter are different, I should return false and move
+ * tested string by one place or remove last letter from it and try for shorter function.
+ *
+ * Easier said than code!
+ */
