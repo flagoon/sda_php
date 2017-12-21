71fb5f2160a5e7badf789b71c440c48afe232f1f
Paweł Kochanek <flagoon@o2.pl>
2017-12-21 21:54:33 +0100

    created iterable fibonacci function

---
+++ iter_fibo.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+system('clear');
+
+function iter_fibo(int $number): float {
+    $prevResult = 0;
+    $nextResult = 1;
+    if($number === 0) {
+        return 0;
+    } elseif ($number === 1) {
+        return 1;
+    } else {
+        for ($i = 0; $i <= $number - 2; $i++) {
+            $result = $prevResult + $nextResult;
+            $prevResult = $nextResult;
+            $nextResult = $result;
+        }
+    }
+    return $result;
+}
+
+echo iter_fibo(3) . PHP_EOL;
