16d481bd04dfe9bbc8ec81f15d6be1cd22d03892
Paweł Kochanek <flagoon@o2.pl>
2017-12-21 21:56:55 +0100

    added some comments

--- iter_fibo.php
+++ iter_fibo.php
@@ -2,9 +2,14 @@
 declare(strict_types=1);
 system('clear');
 
+/**
+ * @param $number integer, number of Fibonacci interations. It returns float as for bigger numbers it's not integer.
+ */
 function iter_fibo(int $number): float {
     $prevResult = 0;
     $nextResult = 1;
+
+    // This conditions are needed for functions with parameter smaller than 2.
     if($number === 0) {
         return 0;
     } elseif ($number === 1) {
