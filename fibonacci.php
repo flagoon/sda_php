9e62548889af59f3f7197e2fadbca6051f05a861
Paweł Kochanek <flagoon@o2.pl>
2017-12-21 22:52:09 +0100

    created fibonacci with recursive function

--- fibonacci.php
+++ fibonacci.php
@@ -2,8 +2,16 @@
 declare(strict_types=1);
 system('clear');
 
+/** @param $number integer number to show fibonacci for this position. */
+/** @param $number integer number to show fibonacci for this position. */
 function fibonacci(int $number): float
 {
+    if($number <= 2) {
+        return 1;
+    }
+
+    return fibonacci($number - 2) + fibonacci($number - 1);
+
     // f(5) = f(4) + f(3);  =>  5
     // f(4) = f(3) + f(2);  =>  3
     // f(3) = f(2) + f(1);  =>  2
@@ -11,4 +19,4 @@ function fibonacci(int $number): float
     // f(1) = 1             =>  1
 }
 
-fibonacci(5);
+echo fibonacci(15) . PHP_EOL;
