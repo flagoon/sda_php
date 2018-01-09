e0365a3a7887d4071c64acb3da30468a907142f2
Paweł Kochanek <flagoon@o2.pl>
2018-01-09 21:40:58 +0100

    Prettier test

--- freewiki/src/Components/App.js
+++ freewiki/src/Components/App.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <div>Hello World</div>
-    );
+    const test = 12;
+    if (test) {
+      const test2 = 19;
+      let ret = 11;
+      console.log(test2 + ret);
+    }
+    return <div>Hello World</div>;
   }
 }
 
