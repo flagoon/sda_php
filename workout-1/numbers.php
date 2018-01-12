7208abb2678f985f690cf94e9e62b4736e6da88b
Paweł Kochanek <flagoon@o2.pl>
2018-01-12 21:29:30 +0100

    Numbers.php with echoing numbers in for loop.

---
+++ workout-1/numbers.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flagoon
+ * Date: 12.01.18
+ * Time: 21:26
+ */
+declare(strict_types=1);
+system("clear");
+
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "{$i}" . PHP_EOL;
+}
