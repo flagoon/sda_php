fd4474e7310634e9fc6b10867e8b4b05546b7839
Paweł Kochanek <flagoon@o2.pl>
2018-01-12 22:09:40 +0100

    Multiply by two.

---
+++ workout-1/two.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flagoon
+ * Date: 12.01.18
+ * Time: 22:07
+ */
+declare(strict_types=1);
+system("clear");
+
+$two = 2;
+for ($i = 1; $i <= 10; $i++) {
+    echo "{$two} * {$i} = " . $two * $i . PHP_EOL;
+}
