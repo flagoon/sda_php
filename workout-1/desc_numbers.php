0ef0720bfcb793f3ba58b1930098fdbe3267d8fe
Paweł Kochanek <flagoon@o2.pl>
2018-01-12 21:31:52 +0100

    Desc number with counting down to 1 with for loop.

---
+++ workout-1/desc_numbers.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flagoon
+ * Date: 12.01.18
+ * Time: 21:30
+ */
+declare(strict_types=1);
+system("clear");
+
+for ($i = 10; $i > 0; $i--) {
+    echo "{$i}" . PHP_EOL;
+}
