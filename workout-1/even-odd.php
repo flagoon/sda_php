ca70e4a2b468d0853c32127dc9751719ed52f366
Paweł Kochanek <flagoon@o2.pl>
2018-01-12 21:58:01 +0100

    script for odd number and even-odd numbers.

---
+++ workout-1/even-odd.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flagoon
+ * Date: 12.01.18
+ * Time: 21:54
+ */
+declare(strict_types=1);
+system("clear");
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "{$i} is ";
+    if($i % 2) {
+        echo "odd number. " . PHP_EOL;
+    } else {
+        echo "even number. " . PHP_EOL;
+    }
+}
---
+++ workout-1/odd.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flagoon
+ * Date: 12.01.18
+ * Time: 21:51
+ */
+
+declare(strict_types=1);
+system("clear");
+
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2) {
+        echo $i . PHP_EOL;
+    }
+}
