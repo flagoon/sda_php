e27e8197eb0dc848ccad48af9a2a7b828b73de9e
Paweł Kochanek <flagoon@o2.pl>
2018-01-12 21:35:05 +0100

    Echo numbers with foreach loop

---
+++ workout-1/foreach.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flagoon
+ * Date: 12.01.18
+ * Time: 21:32
+ */
+
+declare(strict_types=1);
+system("clear");
+
+$numbers = [1,2,3,4,5,6,7,8,9,10];
+foreach ($numbers as $number) {
+    echo $number . PHP_EOL;
+}
