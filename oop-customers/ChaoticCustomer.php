31372767b7d991286213e4e465bb18f65e4d3f25
Paweł Kochanek <flagoon@o2.pl>
2018-01-13 09:57:23 +0100

    oop-customers: added ChaoticCustomer class and replace some stuff in index.

---
+++ oop-customers/ChaoticCustomer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flagoon
+ * Date: 13.01.18
+ * Time: 09:52
+ */
+declare(strict_types=1);
+
+class ChaoticCustomer extends Customer
+{
+
+}
--- oop-customers/index.php
+++ oop-customers/index.php
@@ -2,8 +2,9 @@
 declare(strict_types=1);
 
 require_once 'Customer.php';
+require_once 'ChaoticCustomer.php';
 
-$customer = new Customer();
+$customer = new ChaoticCustomer();
 $customer->setName('Marian');
-echo $customer->getDiscount();
+echo $customer->getDiscount() . PHP_EOL;
 var_export($customer);
