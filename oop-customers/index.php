d5ff379311875bf9963b74a4fdbe50976a3cd98f
Paweł Kochanek <flagoon@o2.pl>
2018-01-13 10:58:48 +0100

    oop-customers: Added gentlecustomer logic.

--- oop-customers/index.php
+++ oop-customers/index.php
@@ -1,8 +1,10 @@
 <?php
 declare(strict_types=1);
+system("clear");
 
 require_once 'Customer.php';
 require_once 'ChaoticCustomer.php';
+require_once 'GentleCustomer.php';
 
 $customer = new Customer();
 $customer->setName('Marian');
@@ -17,5 +19,14 @@ $rudolf = new ChaoticCustomer();
 $rudolf->setName('Stefan');
 echo "\e[31mCustomer: " . $rudolf->getName() . PHP_EOL;
 echo "Discount: " . $rudolf->getDiscount() . PHP_EOL;
-echo $rudolf->askForDiscount() . "\e[0m";
+echo $rudolf->askForDiscount() . "\e[0m" . PHP_EOL;
 var_export($rudolf);
+
+echo PHP_EOL . "<+++++++++++++++++++++++>" . PHP_EOL;
+
+$zenek = new GentleCustomer();
+$zenek->setName('Zenek');
+echo "\e[32mCustomer: " . $zenek->getName() . PHP_EOL;
+echo "Discount: " . $zenek->getDiscount() . PHP_EOL;
+echo $zenek->askForDiscount() . "\e[0m" . PHP_EOL;
+var_export($zenek);
