ac561968f852c060a2af15bd8a43b233f06389bb
Paweł Kochanek <flagoon@o2.pl>
2018-01-13 10:07:01 +0100

    oop-customers: Change chaotic customer in Index.

--- oop-customers/index.php
+++ oop-customers/index.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 require_once 'Customer.php';
 require_once 'ChaoticCustomer.php';
 
-$customer = new ChaoticCustomer();
+$customer = new Customer();
 $customer->setName('Marian');
-echo $customer->getDiscount() . PHP_EOL;
+echo "Customer: " . $customer->getName() . PHP_EOL;
+echo "Discount: " . $customer->getDiscount() . PHP_EOL;
 var_export($customer);
+
+echo PHP_EOL . "<+++++++++++++++++++++++>" . PHP_EOL;
+
+$rudolf = new ChaoticCustomer();
+$rudolf->setName('Marian');
+echo "Customer: " . $rudolf->getName() . PHP_EOL;
+echo "Discount: " . $rudolf->getDiscount() . PHP_EOL;
+var_export($rudolf);
