0c86806b37aa578f9e4479993f4dcfc00031e668
Paweł Kochanek <flagoon@o2.pl>
2018-01-14 21:08:49 +0100

    oop-customers: removed trash to better handle output.

--- oop-customers/index.php
+++ oop-customers/index.php
@@ -4,45 +4,14 @@ system("clear");
 
 require_once 'CustomerInterface.php';
 require_once 'Customer.php';
-require_once 'Invoice.php';
-require_once 'ChaoticCustomer.php';
 require_once 'GentleCustomer.php';
+require_once 'ChaoticCustomer.php';
 require_once 'CustomerFactory.php';
+require_once 'Invoice.php';
 
-$customer = new Customer(10);
-$customer->setName('Marian');
-echo "Customer: " . $customer->getName() . PHP_EOL;
-echo "Discount: " . $customer->getDiscount() . PHP_EOL;
-echo $customer->askForDiscount() . PHP_EOL;
-var_export($customer);
-
-echo PHP_EOL . "<+++++++++++++++++++++++>" . PHP_EOL;
-
-$rudolf = new ChaoticCustomer(101);
-$rudolf->setName('Stefan');
-echo "\e[31mCustomer: " . $rudolf->getName() . PHP_EOL;
-echo "Discount: " . $rudolf->getDiscount() . PHP_EOL;
-echo $rudolf->askForDiscount() . "\e[0m" . PHP_EOL;
-var_export($rudolf);
-
-echo PHP_EOL . "<+++++++++++++++++++++++>" . PHP_EOL;
-
-$zenek = new GentleCustomer(210);
+// create new customer, Zenek.
+$zenek = CustomerFactory::chooseCustomer(CustomerFactory::GENTLE, 1980);
 $zenek->setName('Zenek');
-echo "\e[32mCustomer: " . $zenek->getName() . PHP_EOL;
-echo "Discount: " . $zenek->getDiscount() . PHP_EOL;
-echo $zenek->askForDiscount() . "\e[0m" . PHP_EOL;
-echo $zenek->askForRenew() . PHP_EOL;
-var_export($zenek);
-
-echo PHP_EOL . "<+++++++++++++++++++++++>" . PHP_EOL;
-
-$criss = CustomerFactory::chooseCustomer(CustomerFactory::GENTLE, 2001);
-$criss->setName('Criss');
-echo "\e[35mCustomer: " . $criss->getName() . PHP_EOL;
-echo "Discount: " . $criss->getDiscount() . PHP_EOL;
-echo $criss->askForDiscount() . "\e[0m" . PHP_EOL;
-$criss->setNIP('123-321-23-32');
-$criss->setSurname('Cross');
-$invoice = $criss->askForInvoice(1100);
-var_export($invoice);
+$zenek->setSurname('Wiaderko');
+$zenek->setNIP('12-32-512-112');
+var_export($zenek->askForInvoice(1));
