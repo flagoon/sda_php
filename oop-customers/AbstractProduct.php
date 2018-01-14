f7591eb90b5f99a8fa4f9491e360734b512d7b17
Paweł Kochanek <flagoon@o2.pl>
2018-01-14 13:15:05 +0100

    oop-customers: added methods for AbstractProduct.

--- oop-customers/AbstractProduct.php
+++ oop-customers/AbstractProduct.php
@@ -5,8 +5,22 @@
  * Date: 14.01.18
  * Time: 12:46
  */
+declare(strict_types=1);
+system('clear');
 
 abstract class AbstractProduct
 {
+    protected $price;
 
+    abstract public function order();
+
+    public function setPrice(int $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
 }
