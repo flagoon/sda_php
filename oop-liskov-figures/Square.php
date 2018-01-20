b51e24521b94d02082533c72080751c2d4317649
Paweł Kochanek <flagoon@o2.pl>
2018-01-20 13:12:50 +0100

    oop-liskov-figures: Create Square class.

---
+++ oop-liskov-figures/Square.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flagoon
+ * Date: 20.01.18
+ * Time: 12:36
+ */
+
+class Square extends Rectangle
+{
+    public function setHeight(int $height): void
+    {
+        $this->height = $height;
+    }
+
+    public function setWidth(int $width): void
+    {
+        $this->width = $this->height;
+    }
+}
