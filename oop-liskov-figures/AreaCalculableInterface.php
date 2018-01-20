92cfaae26dbf53a79f47401cd83943663b6cf725
Paweł Kochanek <flagoon@o2.pl>
2018-01-20 14:37:49 +0100

    oop-liskov-figures: created interface for shapes with calculable area.

---
+++ oop-liskov-figures/AreaCalculableInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flagoon
+ * Date: 20.01.18
+ * Time: 14:36
+ */
+declare(strict_types=1);
+
+interface AreaCalculableInterface
+{
+    public function calcArea(): float;
+}
