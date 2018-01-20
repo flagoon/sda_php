713fbb0b271dfec5b4de0b5fcfb66283585b6478
Paweł Kochanek <flagoon@o2.pl>
2018-01-20 14:46:46 +0100

    oop-liskov-figures: added todo

--- oop-liskov-figures/index.php
+++ oop-liskov-figures/index.php
@@ -11,6 +11,8 @@ system("clear");
 require_once 'Rectangle.php';
 require_once 'Square.php';
 
+// TODO: Write function to calc areas of objects
+
 $rect = new Rectangle();
 $rect->setHeight(5);
 $rect->setWidth(3);
@@ -20,17 +22,3 @@ $sqr = new Square();
 $sqr->setHeight(5);
 $sqr->setWidth(3);
 echo "Area of square is: {$sqr->calcAreaOf()}" . PHP_EOL;
-
-
-function getArea(Rectangle $rectangle): int
-{
-    $rectangle->setWidth(2);
-    $rectangle->setHeight(3);
-
-    return $rectangle->calcAreaOf();
-}
-
-$newR = new Rectangle();
-$newS = new Square();
-echo getArea($newR) . PHP_EOL;
-echo getArea($newS) . PHP_EOL;
