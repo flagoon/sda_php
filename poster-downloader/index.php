db2f717734b9283843746d23d16bcb845363919f
Paweł Kochanek <flagoon@o2.pl>
2018-01-27 12:18:13 +0100

    Title handling added.

--- poster-downloader/index.php
+++ poster-downloader/index.php
@@ -10,6 +10,21 @@ require_once './src/Helper.php';
 
 $helper = new Helper;
 
+$titleArray = [
+    'Pirates of the Caribbean Dead Men Tell No Talse',
+    'Rings',
+    'Blade Runner 2049',
+    'Thor',
+    'Get Out',
+    'Star Wars the Last Jedi',
+    'Okja',
+    'London has Fallen',
+    'The Legend of Tarzan',
+    'The Founder',
+    'Captain America Civil War',
+    'Spectre'
+];
+
 $helper->clearImgFolder('posters');
 $helper->clearImgFolder('shots');
 $imgSaveDir = './src/posters';
@@ -23,12 +38,13 @@ for ($i = 1; $i <= 12; $i++) {
     file_put_contents(
         './src/posters.txt',
         $date->format('Y-m-d H:i:s.u')
-        . ' start downloading poster.'
+        . ' start downloading poster of '
+        . $titleArray[$i - 1] . '.'
         . PHP_EOL,
         FILE_APPEND
     );
     $poster = file_get_contents($urlLocationPosters . $i . '.jpg');
-    file_put_contents($imgSaveDir . '/'. $i . '.jpg', $poster);
+    file_put_contents($imgSaveDir . '/'. $titleArray[$i - 1] . '.jpg', $poster);
     $date = new DateTime();
     file_put_contents(
         './src/posters.txt',
@@ -44,12 +60,12 @@ for ($i = 1; $i <= 12; $i++) {
     file_put_contents(
         './src/shots.txt',
         $date->format('Y-m-d H:i:s.u')
-        . ' start downloading poster.'
-        . PHP_EOL,
+        . ' start downloading shot of '
+        . $titleArray[$i - 1] . '.',
         FILE_APPEND
     );
     $shot = file_get_contents($urlLocationShots . $i . '.jpg');
-    file_put_contents($shotsSaveDir . '/'. $i . '.jpg', $shot);
+    file_put_contents($shotsSaveDir . '/'. $titleArray[$i - 1] . '.jpg', $shot);
     $date = new DateTime();
     file_put_contents(
         './src/shots.txt',
@@ -59,7 +75,3 @@ for ($i = 1; $i <= 12; $i++) {
         FILE_APPEND
     );
 }
-
-
-
-//$posterName = basename($urlLocationPosters);
