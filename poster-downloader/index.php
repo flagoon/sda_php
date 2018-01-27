2f8d7060ada2d7de6d7bdf51f42aefc01048a15b
Paweł Kochanek <flagoon@o2.pl>
2018-01-27 11:31:59 +0100

    added writing logs to txt files

--- poster-downloader/index.php
+++ poster-downloader/index.php
@@ -11,13 +11,55 @@ require_once './src/Helper.php';
 $helper = new Helper;
 
 $helper->clearImgFolder('posters');
+$helper->clearImgFolder('shots');
 $imgSaveDir = './src/posters';
+$shotsSaveDir = './src/shots';
 
-$urlLocation = 'https://cytaty.eu/img/sda/posters/';
+$urlLocationPosters = 'https://cytaty.eu/img/sda/posters/';
+$urlLocationShots = 'https://cytaty.eu/img/sda/shots/';
 
 for ($i = 1; $i <= 12; $i++) {
-    $poster = file_get_contents($urlLocation . $i . '.jpg');
+    $date = new DateTime();
+    file_put_contents(
+        './src/posters.txt',
+        $date->format('Y-m-d H:i:s.u')
+        . ' start downloading poster.'
+        . PHP_EOL,
+        FILE_APPEND
+    );
+    $poster = file_get_contents($urlLocationPosters . $i . '.jpg');
     file_put_contents($imgSaveDir . '/'. $i . '.jpg', $poster);
+    $date = new DateTime();
+    file_put_contents(
+        './src/posters.txt',
+        $date->format('Y-m-d H:i:s.u')
+        . ' download stopped.'
+        . PHP_EOL,
+        FILE_APPEND
+    );
 }
 
-//$posterName = basename($urlLocation);
+for ($i = 1; $i <= 12; $i++) {
+    $date = new DateTime();
+    file_put_contents(
+        './src/shots.txt',
+        $date->format('Y-m-d H:i:s.u')
+        . ' start downloading poster.'
+        . PHP_EOL,
+        FILE_APPEND
+    );
+    $shot = file_get_contents($urlLocationShots . $i . '.jpg');
+    file_put_contents($shotsSaveDir . '/'. $i . '.jpg', $shot);
+    $date = new DateTime();
+    file_put_contents(
+        './src/shots.txt',
+        $date->format('Y-m-d H:i:s.u')
+        . ' download stopped.'
+        . PHP_EOL,
+        FILE_APPEND
+    );
+}
+
+
+
+//$posterName = basename($urlLocationPosters);
