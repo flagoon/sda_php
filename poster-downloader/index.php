9f9ad5f656e9a7a061ee3094ae693348bf1793b2
Paweł Kochanek <flagoon@o2.pl>
2018-01-27 10:43:47 +0100

    Multiple file download

--- poster-downloader/index.php
+++ poster-downloader/index.php
@@ -6,17 +6,18 @@
  * Time: 09:34
  */
 
-require __DIR__ . '/vendor/autoload.php';
+require_once './src/Helper.php';
 
 $helper = new Helper;
 
 $helper->clearImgFolder('posters');
-
-$urlLocation = 'https://upload.wikimedia.org/wikipedia/en/4/4f/The_Hobbit_-_The_Desolation_of_Smaug_theatrical_poster.jpg';
-
-$poster = file_get_contents($urlLocation);
-$posterName = basename($urlLocation);
 $imgSaveDir = './src/posters';
 
-file_put_contents($imgSaveDir . '/'. $posterName, $poster);
+$urlLocation = 'https://cytaty.eu/img/sda/posters/';
 
+for ($i = 1; $i <= 12; $i++) {
+    $poster = file_get_contents($urlLocation . $i . '.jpg');
+    file_put_contents($imgSaveDir . '/'. $i . '.jpg', $poster);
+}
+
+//$posterName = basename($urlLocation);
