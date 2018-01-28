47501a712bdc457081b2e3086e54d44df28ee507
Paweł Kochanek <flagoon@o2.pl>
2018-01-28 15:15:01 +0100

    Documentation and spelling mistakes.

--- poster-downloader/src/Flagoon/Config.php
+++ poster-downloader/src/Flagoon/Config.php
@@ -14,16 +14,35 @@ namespace Flagoon;
  */
 class Config
 {
-    public const SHOTS_LOG_FILE = "./logs/postersLog.txt";
+    /**
+     * @var string for logs for shots.
+     */
+    public const SHOTS_LOG_FILE = "./logs/shotsLog.txt";
+    /**
+     * @var string for directory for shots.
+     */
     public const SHOTS_SAVE_DIR = "./resources/shots/";
+    /**
+     * @var string location from where we get shots.
+     */
     public const SHOTS_URL = "https://cytaty.eu/img/sda/shots/";
-
+    /**
+     * @var string for logs for posters.
+     */
     public const POSTER_LOG_FILE = "./logs/shotsLog.txt";
+    /**
+     * @var string for directory for posters.
+     */
     public const POSTERS_SAVE_DIR = "./resources/posters/";
+    /**
+     * @var string location from where we get posters.
+     */
     public const POSTERS_URL = "https://cytaty.eu/img/sda/posters/";
-
+    /**
+     * @var string[] holds titles of movies.
+     */
     public const TITLES_ARRAY = [
-        "Pirates of the Caribbean: Dead Men Tell No Talse",
+        "Pirates of the Caribbean: Dead Men Tell No Tales",
         "Rings",
         "Blade Runner 2049",
         "Thor",
